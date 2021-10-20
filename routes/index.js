755504711930263806edcbe539e5a934de2da71f
Pranav Kandarpa <pranavkandarpa@Pranavs-MacBook-Pro-2.local>
2021-10-20 10:12:40 -0400

    Adding initial routes

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const GetBlogs = require('./controller/getBlogs')
 const PostBlog = require('./controller/postBlog')
+const GetComment = require('../controller/getComment')
+const PostComment = require('../controller/postComment')
 
 const router = express.Router()
 
 router.get('/blog/:id', GetBlogs)
 router.post('/blog/post', PostBlog)
+//router.get('/blog/:id/comment', GetComment)
+//router.post('/blog/:id/post', PostComment)
 
 module.exports = router
