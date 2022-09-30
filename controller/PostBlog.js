c1d8a517d6df9f3301dd5b9c02eb3d0629eb88e6
unknown <jmcc986@gmail.com>
2022-09-29 20:10:24 -0400

    Refactor of post route

--- controller/PostBlog.js
+++ controller/PostBlog.js
@@ -2,6 +2,11 @@ const Blog = require('../models/blog')
 
 const postBlog = async (req, res) => {
   const blog = await Blog.create(req.body);
+
+  res.status(201).json({
+    success: true,
+    data: blog
+})
 }
 
 module.exports = { postBlog }
